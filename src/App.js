723a2e8aea1141951819be72e622170f12d6f3b4
Jai7-Y <jaipyadav07@gmail.com>
2024-02-05 01:43:38 +0530

    Added use state for any change in input feild

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
-import {useState} from "react"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  // console.log(todo);
+
+  // handleChange()
+
   return (
     <div className="App">
       <h1>React Todo App</h1>
 
-      <div className='input-wrapper'>
-        <input type='text' name='todo' placeholder='Create a new todo'></input>
-        <button className='add-button'>Add</button>
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create a new todo"
+          onChange={(e) => {
+            console.log(todo)
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="add-button">Add</button>
       </div>
     </div>
   );
