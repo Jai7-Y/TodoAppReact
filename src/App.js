0c73bf6dd90f6c45328e8f27c6437e6bf84b9fdb
Jai7-Y <jaipyadav07@gmail.com>
2024-02-05 01:15:02 +0530

    Created the basic imput feild and mockup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {useState} from "react"
 
 function App() {
   return (
     <div className="App">
-      "Jai Shree Ram"
+      <h1>React Todo App</h1>
+
+      <div className='input-wrapper'>
+        <input type='text' name='todo' placeholder='Create a new todo'></input>
+        <button className='add-button'>Add</button>
+      </div>
     </div>
   );
 }
